7280b8f6efaf91e53b83e871b007c9a3d3417192
Görkem Öztürk <gorkemozturk@hotmail.com>
2018-11-28 18:41:28 +0300

    Management template changed, and identity implemented.

---
+++ BlogOn/Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogOn.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Authorized")]
+        public bool IsAuthorized { get; set; }
+
+        [Display(Name = "Created At")]
+        public DateTime CreatedAt { get; set; }
+
+        public string FullName() {
+            return FirstName + " " + LastName;
+        }
+    }
+}
