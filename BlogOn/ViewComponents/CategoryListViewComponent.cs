c15a3ba979946b7ec7f30081d198985f00fabc67
Görkem Öztürk <gorkemozturk@hotmail.com>
2018-12-01 14:01:02 +0300

    Category list view component has implemented and located in the home view.

---
+++ BlogOn/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using BlogOn.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogOn.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryListViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.Categories.Where(c => c.IsActive == true).ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
